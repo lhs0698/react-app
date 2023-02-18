744efdb3e99a2a2191f06b00060f407906c710f3
lhs0698 <yohs0698@naver.com>
2023-02-18 19:55:55 +0900

    MainPage css 적용

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,12 +5,11 @@ import { Offcanvas } from "react-bootstrap";
 import { BsJustify } from "react-icons/bs";
 
 import styled from "styled-components";
+import '../style/MainPage.css'
 
 const MainContainer = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: #CCFFFF;
-`
+  background-color: #34495e;
+`;
 
 function Main() {
   const [show, setShow] = useState(false);
@@ -19,25 +18,30 @@ function Main() {
   const handleShow = () => setShow(true);
 
   return (
-    <MainContainer>
-      <BsJustify size="27" onClick={handleShow}/>
-      <Offcanvas show={show} onHide={handleClose}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>하고 싶은거</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          <Link to="/counter">
-            <div>counter</div>
-          </Link>
-          <Link to="/memo">
-            <div>Memo</div>
-          </Link>
-          <Link to="/Users">
-            <div>Users_Json OpenAPI</div>
-          </Link>
-        </Offcanvas.Body>
-      </Offcanvas>
-    </MainContainer>
+      <MainContainer>
+        <BsJustify size="35" onClick={handleShow} />
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>하고 싶은거</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Link to="/counter">
+              <div>counter</div>
+            </Link>
+            <Link to="/memo">
+              <div>Memo</div>
+            </Link>
+            <Link to="/Users">
+              <div>Users_Json OpenAPI</div>
+            </Link>
+          </Offcanvas.Body>
+        </Offcanvas>
+        <div className="body">
+          <div className="loading">
+            <span>loading...</span>
+          </div>
+        </div>
+      </MainContainer>
   );
 }
 
